ca3b7fd59e1f47e709c84b68146304f633e38fe6
zhuyijing <zhuyijing168@163.com>
2016-09-21 17:42:48 +0800

    add status check

--- main.py
+++ main.py
@@ -4,7 +4,10 @@
 from flask import Flask, g, request
 from weixin.micros import WeixinAPI
 from transaction.robot import RobotAPI
+from jinja2 import Environment, FileSystemLoader
 import logging
+import os
+import commands
 
 app = Flask(__name__)
 
@@ -14,6 +17,30 @@ TOKEN = 'wexin123'
 def hello():
     msg = request.args.get('name', 'bob')
     return "Hello, %s! - Flask" % msg
+
+@app.route('/status')
+def check_status():
+    THIS_DIR = os.path.dirname(os.path.abspath(__file__))
+    env = Environment(loader=FileSystemLoader(THIS_DIR))
+    template = env.get_template('weixin/template/status.tpl')
+    items = []
+    
+    result= commands.getstatusoutput('supervisorctl status wxpub')
+    print result
+    up = True if 'RUNNING' in result[1] else False
+    it = {'service': u'公众号服务', 'up': up}
+    items.append(it)
+    result= commands.getstatusoutput('supervisorctl status wxschtask')
+    print result
+    up = True if 'RUNNING' in result[1] else False
+    it = {'service': u'公众号定时推送', 'up': up}
+    items.append(it)
+    result= commands.getstatusoutput('supervisorctl status iss')
+    print result
+    up = True if 'RUNNING' in result[1] else False
+    it = {'service': u'虚拟网服务', 'up': up}
+    items.append(it)
+    return template.render(items=items)
 
 @app.route('/weixin', methods = ['GET', 'POST'])
 def wechat():
@@ -38,4 +65,4 @@ def wechat():
     return wxapi.reply(res['msgtype'], res['content'], msgbody['from'], msgbody['to'])
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=8080, debug=False)
+    app.run(host='0.0.0.0', port=8000, debug=False)
---
+++ weixin/template/status.tpl
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width,initial-scale=1,user-scalable=0">
+        <title>WeUI</title>
+        <!-- 引入 WeUI -->
+        <link rel="stylesheet" href="https://res.wx.qq.com/open/libs/weui/0.4.2/weui.css"/>
+    </head>
+    <body>
+        <!-- 使用 -->
+        
+        <div class="weui_cells_title">当前服务状态检测</div>
+    <div class="weui_cells">
+        {% for it in items %}
+        <div class="weui_cell">
+            <div class="weui_cell_hd">
+              {% if it.up %}
+                <i class="weui_icon_success"></i>
+              {% else %}
+                <i class="weui_icon_warn"></i>
+              {% endif %}
+            </div>
+            <div class="weui_cell_bd weui_cell_primary">
+                <p>&nbsp;&nbsp;{{ it.service }}</p>
+            </div>
+            <div class="weui_cell_ft">
+                    {% if it.up %}
+                        服务在线
+                    {% else %}
+                        服务离线
+                    {% endif %}
+            </div>
+        </div>
+        {% endfor %}
+    </div>
+    </body>
+</html>
